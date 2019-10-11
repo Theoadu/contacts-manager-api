a301b72926a5b3610adcc979b8f3a2dc169bc748
Theophilus E.K Adukpo <theoadu@gmail.com>
2019-10-11 04:52:09 +0000

    Added DB connection and configured it

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const logger = require('morgan');
 const fs = require('fs');
 const path = require('path');
 
+const connectDB = require('./config/db');
 // Import all the routes files here
 const usersRoute = require('./routes/users');
 const authRoute = require('./routes/auth');
 const contactsRoute = require('./routes/contacts');
 
 const app = express();
+
+// Connect to MongoDB here
+connectDB();
 
 app.use(logger('dev'));
 
@@ -23,6 +27,6 @@ app.use('/api/v1/users', usersRoute);
 app.use('/api/v1/contacts', contactsRoute);
 app.use('/api/v1/auth', authRoute);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
