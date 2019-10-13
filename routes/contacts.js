62f71eb6e02df985135134c46d38d732b6c46549
Theophilus E.K Adukpo <theoadu@gmail.com>
2019-10-13 20:20:53 +0000

    Added contact model

--- routes/contacts.js
+++ routes/contacts.js
@@ -17,8 +17,16 @@ router.put("/:id", (req, res) => {
   res.send("Update contacts here");
 });
 
-router.get("/", (req, res) => {
-  res.send("Get All contact here");
+router.get("/", auth, async (req, res) => {
+  try {
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: "-1"
+    });
+    res.status(200).json(contacts);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ msg: "Server Error" });
+  }
 });
 
 module.exports = router;
