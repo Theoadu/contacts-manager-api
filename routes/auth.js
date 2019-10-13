6e1ddd9a7ab6935eeebca72654ada463a1e51fed
Theophilus E.K Adukpo <theoadu@gmail.com>
2019-10-13 19:56:39 +0000

    Added auth middleware

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,68 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator");
+
+const auth = require("../middleware/auth");
+const User = require("../models/User");
 
 // @Route       POST api/auth
 // @Desc        Login a user
 // @Access      Public
-router.post('/', (req, res)=>{
-    res.send('Register a user');
-})
+router.post(
+  "/",
+  [
+    check("email", "Please enter a valid email").isEmail(),
+    check("password", "Password is require").exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ msg: "Invalid user credentials" });
+      }
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ msg: "Invalid user credentials" });
+      }
+      const jwtpayload = {
+        user: {
+          id: user.id
+        }
+      };
+      jwt.sign(
+        jwtpayload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      if (error) throw error;
+      res.status(500).json({ msg: "Server Error" });
+    }
+  }
+);
 
-router.get('/', (req, res)=>{
-    res.send('Register a user');
-})
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ msg: "Server Error" });
+  }
+});
 
 module.exports = router;
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,19 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+
+const auth = require("../middleware/auth");
+const User = require("../models/User");
+const Contact = require("../models/Contact");
 
 // @Route       POST api/contacts
 // @Desc        Registers new contact
 // @Access      Private
-router.post('/', (req, res)=>{
-    res.send('Register a user');
-})
+router.post("/", (req, res) => {
+  res.send("Register a user");
+});
 
-router.put('/:id', (req, res)=> {
-    res.send('Update contacts here')
-})
+router.put("/:id", (req, res) => {
+  res.send("Update contacts here");
+});
 
-router.get('/', (req, res)=>{
-    res.send('Get All contact here')
-})
+router.get("/", (req, res) => {
+  res.send("Get All contact here");
+});
 
 module.exports = router;
