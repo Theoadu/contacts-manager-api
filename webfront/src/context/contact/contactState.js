7ae1c66ae29f12c7562e8abe0bada3a8924a5177
Theophilus E.K Adukpo <theoadu@gmail.com>
2019-10-15 04:34:52 +0000

    Added context and configured

---
+++ webfront/src/context/contact/contactState.js
@@ -0,0 +1,5 @@
+import React, {useReducer} from 'react';
+import uuid from 'uuid';
+import contactContext from './contactContext';
+import contactReducer from './contactReducer';
+import {} from '../types';
